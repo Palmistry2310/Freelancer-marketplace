99e84eda5de21edec5a7750efafc02c74a7e3906
Palmistry2310 <palmistry017@gmail.com>
2025-01-08 19:22:54 +0530

    established jobroutes,model,controller

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -2,9 +2,20 @@ const Job= require("../models/Job")
 
 exports.getAllJobs= async(req,res)=>{
 
+        const jobs= await Job.find({},{_id:0,__v:0})
+        res.render("job/jobList",{jobs})
     
+
 }
 
 exports.createJob=async(req,res)=>{
+
+    const { title, desc, skillsRequired, deadline, budget } = req.body;
+    const skillArray=skillsRequired.split(",").map(skill=>skill.trim())
+    const newJob= new Job({ title, desc, skillsRequired:skillArray, deadline, budget })
+
+    await newJob.save()
+    res.send("job added")
+
 
 }
--- models/Job.js
+++ models/Job.js
@@ -1,14 +1,13 @@
 const mongoose=require("mongoose")
 
 const jobSchema= new mongoose.Schema({
-    title:{type:String, required:true},
+    title : {type:String, required:true},
     desc:{type:String, required:true},
     skillsRequired:[String],
     deadline:{type:Date, required:true},
     budget:{type:Number,required:true}
 })
 
-const Job=mongoose.model("Job","jobSchema");
+const Job=mongoose.model("Job",jobSchema);
 
 module.exports = Job;
-

4b66a9d47f9dd3d404c9048ee2059760bab0e5ee
Palmistry2310 <palmistry017@gmail.com>
2025-01-13 20:46:20 +0530

    made frontend of createjob page

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -9,13 +9,15 @@ exports.getAllJobs= async(req,res)=>{
 }
 
 exports.createJob=async(req,res)=>{
-
+    // console.log(req.body)
+    try{
     const { title, desc, skillsRequired, deadline, budget } = req.body;
     const skillArray=skillsRequired.split(",").map(skill=>skill.trim())
     const newJob= new Job({ title, desc, skillsRequired:skillArray, deadline, budget })
-
     await newJob.save()
-    res.send("job added")
-
+    res.status(200).json({message:"Job Posted"})
+    }catch{
+        res.status(500).json({message:"server error, try later!"})
+    }
 
 }
